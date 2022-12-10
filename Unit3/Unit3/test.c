98818734cd385a0767cdd136538ab0b87f6f2e8f
singlechipwang <1361095499@qq.com>
2022-12-10 18:32:20 +0800

    提交代码

---
+++ Unit3/Unit3/test.c
@@ -0,0 +1,22 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+
+#include <stdio.h>
+/*--------------------枚举类型------------------------------*/
+//enum Liquid
+//{
+//	QUNCE = 1,
+//    CUP = 8,
+//	PINT = 16,
+//	QUART//这里未指定的值会自动加1
+//};
+//enum Liquid jar;
+//int main() 
+//{
+//	jar = QUART;//jar是一个枚举类型，实际的值为整型
+//	printf("%d\n",jar);//17
+//	jar = QUART + CUP;
+//	printf("%d\n",jar);//25
+//	return 0;
+//}
+
+
